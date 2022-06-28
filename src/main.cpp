179f9f159455d62e66632984afadfdad003f53c0
bgaechter <bgaechter@users.noreply.github.com>
2022-06-28 11:26:23 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+﻿// chlaus-cpp-template.cpp : Defines the entry point for the application.
+//
+
+#include "chlaus-cpp-template.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
